13bfb35ae6ce65258b55d6b0af34b6f7a2c7997a
Christopher Aument (he/him) <125318094+csaument@users.noreply.github.com>
2023-06-05 12:25:50 -0700

    pushing latest code to github to clear local

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,4 +1,95 @@
 <h1>Experience</h1>
-<p>Personal journey</p>
-<p>Education</p>
-<p>Work</p>
+<h2>Personal journey</h2>
+<p>Whoami?</p>
+<h2>Work</h2>
+<h2>Education</h2>
+<h3>LEARN Academy</h3>
+<p>
+	Mastered front-end and back-end basics during 640 hours of coding experience using React, Ruby on
+	Rails, and GitHub. Employed the git framework for collaborative development of dynamic
+	applications
+</p>
+<ul>
+	<li>
+		Employed test-driven development of full-stack applications with JavaScript, Ruby, Rails, and
+		React.
+	</li>
+	<li>
+		As tech lead during capstone project, managed GitHub codebase, supervised 44 PR mergers, and
+		ensured 100% Jest front-end and RSpec backend test coverage for monolithic application.
+	</li>
+	<li>
+		Volunteered to mentor classmates and JumpStart Weekend students with code review and development
+		during 60 hours of support.
+	</li>
+</ul>
+<h3>Service Schools</h3>
+<h4>Fire Support Coordinator Development Course</h4>
+<h4>Advanced Maneuver Warfare Course</h4>
+<h4>Naval War College, Command and Staff</h4>
+<h4>Expeditionary Warfare School</h4>
+<h4>Joint Humanitarian Operations Course</h4>
+<h4>Mountain Operations Staff Planners Course</h4>
+<h4>Infantry Officers Course</h4>
+<h4>The Basic School</h4>
+<h3>United States Naval Academy</h3>
+<ul>
+	<li>Bachelors of Science in Systems Engineering, with honors</li>
+	<li>Minors in French and Arabic</li>
+	<li>Completed 159 credit hours of coursework in engineering, leadership, and naval science</li>
+	<li>
+		Completed 6-week study abroad program in Morocco, studying French and Culture through a
+		University of Virginia program
+	</li>
+	<li>
+		Capstone project focused on comparison of optical character recognition algorithms for
+		pre-segmented Arabic script within MATLAB and batch processing
+	</li>
+	<li>
+		Conducted sociolinguistic research into variations of North African dialects of spoken Arabic
+	</li>
+	<li>
+		Member of Tau Beta Pi, Middle Eastern Studies Association, Midshipmen African Studies
+		Association, Asia Forum, and Irregular-Emerging Warfare Group
+	</li>
+	<li>Graduated magna cum laude</li>
+</ul>
+<h2>Awards</h2>
+<h2>Certifications</h2>
+<ul>
+	<ul>
+		Engineer in Training
+		<li>Maryland Board of Professional Engineers (NCEES)</li>
+		<li>1292731-408234-e121314</li>
+	</ul>
+	<ul>
+		Victim Advocate
+		<li>Department of Defense Sexual Assault Certification Program</li>
+		<li>BX-1736-8833</li>
+	</ul>
+	<ul>
+		Martial Arts Instructor
+		<li>Martial Arts Center for Excellence</li>
+	</ul>
+	<ul>
+		Joint Humanitarian Operations
+		<li>Expeditionary Warfare Training Group Pacific</li>
+	</ul>
+	<ul>
+		Antiterrorism Officer
+		<li>Headquarters, Marine Corps, Mission Assurance Branch</li>
+	</ul>
+	<ul>
+		Operations Security Officer
+		<li>Interagency OPSEC Support Staff</li>
+	</ul>
+	<ul>
+		Operations and Tactics Instructor
+		<li>Marine Corps Tactics and Operations Group</li>
+	</ul>
+	<li>Lean/Six Sigma White Belt</li>
+	<ul>
+		Fire Support Coordinator
+		<li>Expeditionary Warfare Training Group Atlantic</li>
+	</ul>
+</ul>
